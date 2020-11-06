11fc12ba4d9cb05a673feda0bd43e07b4e262e21
Dmitry Vengerov <hardbeat34@gmail.com>
2020-11-06 16:24:07 +0300

    Add template for Interface segrigation

---
+++ I/main.rb
@@ -0,0 +1,9 @@
+# =============================================
+# Interface Segregation Principle
+# 
+# Clients should not be forced to depend upon
+# interfaces thah thay do not use
+#
+# Robert C.Martion
+# =============================================
+
